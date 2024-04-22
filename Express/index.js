563914ae33badb362846d27eb073055490a23045
Nikeeta26 <151011330+Nikeeta26@users.noreply.github.com>
2024-04-22 20:58:01 +0530

    Add files via upload

--- Express/index.js
+++ Express/index.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`app is listen on port ${port}`);
+ });
+app.set("view engine","ejs");
+
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/:username",(req,res)=>{
+    let{username}=req.params;
+  res.render("home.ejs");
+});
 
